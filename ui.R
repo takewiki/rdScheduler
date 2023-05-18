5051b65e5cc1d00e350768dcc05942d281162e1b
takewiki <hulilei@takewiki.com.cn>
2023-05-19 00:44:25 +0800

    v0.4

--- ui.R
+++ ui.R
@@ -4,23 +4,9 @@ source("ui-helpers.R")
 fluidPage(
   title = "rdScheduler-棱星项目管理软件",
   tags$head(
-    tags$link(href = "style.css", rel = "stylesheet"),
+    tags$link(href = "style.css", rel = "stylesheet")
 
- 
-    # Facebook OpenGraph tags
-    tags$meta(property = "og:title", content = 'rdScheduler-棱星项目管理软件'),
-    tags$meta(property = "og:type", content = "website"),
-    tags$meta(property = "og:url", content = share$url),
-    tags$meta(property = "og:image", content = share$image),
-    tags$meta(property = "og:description", content = share$description),
 
-    # Twitter summary cards
-    tags$meta(name = "twitter:card", content = "summary"),
-    tags$meta(name = "twitter:site", content = paste0("@", share$twitter_user)),
-    tags$meta(name = "twitter:creator", content = paste0("@", share$twitter_user)),
-    tags$meta(name = "twitter:title", content = share$title),
-    tags$meta(name = "twitter:description", content = share$description),
-    tags$meta(name = "twitter:image", content = share$image)
   ),
   tags$a(
     href="https://github.com/takewiki/rdScheduler",
@@ -30,14 +16,7 @@ fluidPage(
   ),
   div(id = "header",
     div(id = "title",
-      "rdScheduler"
-    ),
-    div(id = "subtitle",
-        "棱星项目管理排产软件"),
-    div(id = "subsubtitle",
-        "Copyright @2023 By ",
-        tags$a(href = "http://reshapedata.com/", "上海棱星数据技术有限公司"),
-        HTML("&bull;")
+      "rdScheduler-棱星项目排产管理软件"
     )
   ),
   tabsetPanel(
